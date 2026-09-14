876901cfdeae72ee94320fb03e578ab4340cfc0c
ZhukvK <knst.zhukov@mail.ru>
2026-09-14 19:16:38 +0300

    feat: returned initial tests for filterByKeys

--- test/filterByKeys.js
+++ test/filterByKeys.js
@@ -33,6 +33,30 @@ const testSpecialValues = {
 
 QUnit.module('Тестируем функцию filterByKeys', function () {
 
+    QUnit.test('Работает правильно с простыми объектами', (assert) => {
+        const originalObject = { a: 1, b: 2, c: 3 };
+        const keysToFilter = ['a', 'c'];
+        const result = filterObjectByKeys(originalObject, keysToFilter);
+
+        assert.deepEqual(result, { a: 1, c: 3 }, 'Объект должен содержать только указанные ключи');
+    });
+
+    QUnit.test('Работает правильно с вложенными объектами', (assert) => {
+        const originalObject = { a: 1, b: { c: 2, d: 3 }, e: 4 };
+        const keysToFilter = ['b', 'e'];
+        const result = filterObjectByKeys(originalObject, keysToFilter);
+
+        assert.deepEqual(result, { b: { c: 2, d: 3 }, e: 4 }, 'Вложенные объекты должны быть скопированы');
+    });
+
+    QUnit.test('Работает правильно отсутствующими ключами', (assert) => {
+        const originalObject = { a: 1, b: 2 };
+        const keysToFilter = ['a', 'c']; // 'c' отсутствует
+        const result = filterObjectByKeys(originalObject, keysToFilter);
+
+        assert.deepEqual(result, { a: 1 }, 'Отсутствующие ключи должны быть проигнорированы');
+    });
+
     QUnit.test('Клонирует пустой объект', function (assert) {
         assert.deepEqual(filterByKeys(emptyTestObject, ["name"]), {}, 'filterByKeys({}, ["name"]) === {}');
         assert.deepEqual(filterByKeys(emptyTestObject, []), {}, 'filterByKeys({}, []) === {}');
